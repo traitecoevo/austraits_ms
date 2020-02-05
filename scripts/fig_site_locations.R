19393860db4289c71c71f4ffde59bafaf18270ea
dindiarto <dony.indiarto@gmail.com>
2020-02-05 18:35:36 +1100

    - update fig2a with the latest austraits version - apply  ggpointdensity and viridis color scheme

--- scripts/fig_site_locations.R
+++ scripts/fig_site_locations.R
@@ -1,54 +1,68 @@
 ## Austraits: site locations plot
-
 library(raster)
 library(tidyverse)
 library(ggplot2)
 library(RColorBrewer)
+library(viridis)
+library(ggpointdensity)
 source("R/plotting_functions.R")
 
 
 
 #### 01 Get climate data for Australia ####
 # Download bioclim data using library (raster)
-au_map<-raster("data/australia.tif") %>% 
- # aggregate(fact=6) %>% 
-  as.data.frame(xy=T)
+
+# one strategy for retrieving data available on web
+filename <- "data/australia.tif"
+if (!file.exists(filename))
+  download.file("htpp:....", filename)
+
+# Read bioclim data using raster package
+au_map <- raster(filename) %>%
+  # aggregate(fact=6) %>%
+  as.data.frame(xy = T)
 
 
 #### 02 Plot AU basemap ####
-ggplot()+
-  geom_raster(data=au_map, aes(x = x, y = y, fill=factor(australia)))+
-  scale_fill_brewer(name="",type = "seq", palette = "#800000", guide=F)+xlab("")+ylab("")+ myTheme-> au_basemap
-#au_basemap
+ggplot() +
+  geom_raster(data = au_map, aes(
+    x = x,
+    y = y,
+    fill = factor(australia),
+    show.legend = FALSE)) +
+  myTheme -> au_basemap
+
+au_basemap
 
 #### 03 Overlay site locations  ####
-sites<-read_csv("data/austraits_sites.csv")
-austraits_site_locations<-au_basemap+
-  geom_point(data=sites, aes(y=`latitude (deg)`,
-                              x=`longitude (deg)`, size=n),
-              alpha=0.3, inherit.aes = F)+
-  scale_size(name   = "N Species",
-             breaks = sqrt(fivenum(sites$n)),
-             labels = fivenum(sites$n))
-
-# austraits_site_locations
-# 
-# # #### 04 Save plots ####
-# ggsave("austraits_sites_distribution.png",
-#        austraits_site_locations,  
-#        height=4, width=6, units="in")
+#sites <- read_csv("data/austraits_sites.csv")
+source("R/get_sites.R")
+## Filter sites
+sites %>% 
+  filter(`latitude (deg)` > (-45),
+         `latitude (deg)` < (-9.5),
+         `longitude (deg)` < (153),
+         `longitude (deg)` > (110)) %>% 
+  filter(!site_name %in%c("site_at_-17.4167_degS_and_151.8833_degE",
+                          "site_at_-16.5833_degS_and_150.3167_degE",
+                          "site_at_-16.9333_degS_and_149.1833_degE",
+                          "site_at_-16.9833_degS_and_149.8833_degE")) -> sites2
 
 
 
 #### 03 Overlay site locations  ####
-austraits_site_locations<-au_basemap+
-  geom_point(data=sites, aes(y=`latitude (deg)`,
-                              x=`longitude (deg)`, color=n), 
-              alpha=0.3, inherit.aes = F, size=2)+
-  scale_color_gradient(name="Species (n)", trans="log10", low='#F4A460', high='#800000') +
-  theme(legend.justification = c(0, 0), legend.position = c(0.05, 0.05), legend.direction  = "horizontal")
-austraits_site_locations
+austraits_site_locations <- au_basemap +
+  geom_pointdensity(data = sites2,
+                    aes(y = `latitude (deg)`,
+                        x = `longitude (deg)`
+                    ),
+                    inherit.aes = FALSE,
+                    show.legend = TRUE,
+                    adjust=1) +
+  scale_color_viridis(option="plasma")+
+  theme(legend.justification = c(-0.1, 0), 
+        legend.position = c(0.05, 0.05), 
+        legend.direction  = "horizontal")+
+  scale_fill_grey(name="", start = 0.8,guide=FALSE, na.value = "white")
 
-# ggsave("austraits_sites_distribution_v2.png",
-#        austraits_site_locations,  
-#        height=4, width=5, units="in")
+
